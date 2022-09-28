4eec03b0bbdad9aca775e9e56590ca219fba6a5d
Abigail Forbes <abigail.forbes35@thehutgroup.com>
2022-09-28 09:08:07 +0100

    Problem 1 solution from Euler Project

---
+++ Problem_1.py
@@ -0,0 +1,14 @@
+#Find the sum of all the multiples of 3 or 5 below 1000
+
+sum_of_multiples = 0
+
+for number in range (0, 1000):
+	if number%3 == 0 or  number%5 == 0:
+		sum_of_multiples = sum_of_multiples + number	
+		
+print (sum_of_multiples)
+
+	
+
+
+
